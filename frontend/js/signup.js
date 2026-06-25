57db71de0d96e79ded9d0cee146959acf1494f68
Aswitha-03 <aswithabalu03102006@gmail.com>
2026-06-25 15:15:35 +0530

    Change signup API URL to production
    Updated signup API endpoint from local to production.

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
 
-            const response = await fetch("http://127.0.0.1:5000/signup", {
+            const response = await fetch("https://ai-flash-generator.onrender.com/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
